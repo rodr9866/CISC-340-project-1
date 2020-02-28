00d10f0cf79b23b73e49e940e884befe7d3cac41
biel9436 <61602444+biel9436@users.noreply.github.com>
2020-02-28 15:54:36 -0600

    Add files via upload
    getopt done

--- assembler.c
+++ assembler.c
@@ -1,1 +1,47 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void assembler(char *inputFile, char *outputFile){
+	
+}
+
+int main (int argc, char **argv){
+	char *inputFile = NULL;
+	char *outputFile = NULL;
+	int index;
+	int c;
+	opterr = 0;
+
+	while ((c = getopt (argc, argv, "i:o:")) != -1){
+		switch (c) {
+			case 'o':
+				outputFile = optarg;
+				break;
+			case 'i':
+				inputFile  = optarg;
+				break;
+			case '?':
+				if (optopt == 'i'|| optopt == 'o'){
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				}else if (isprint (optopt)){
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				}else{
+					fprintf (stderr,"Unknown option character `\\x%x'.\n",optopt);
+				}
+				return 1;
+			default:
+				abort ();
+		}
+	}
+	if(inputFile == NULL){
+		fprintf(stderr, "-i  must be included\n");
+		return 1;
+	}
+	printf("outputfile is %s\n", outputFile);
+	printf("Inputfile is %s\n", inputFile);
+}
+
 
