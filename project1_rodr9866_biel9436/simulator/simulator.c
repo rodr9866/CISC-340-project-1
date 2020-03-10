160331891001818a8306eb9547a49b4b26e113ee
rodr9866 <60794335+rodr9866@users.noreply.github.com>
2020-03-10 12:44:46 -0500

    sim

---
+++ project1_rodr9866_biel9436/simulator/simulator.c
@@ -0,0 +1,41 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+	char *inputFile = NULL;
+	char *outputFile = NULL;
+	int index;
+	int c;
+	opterr = 0;
+
+	while ((c = getopt (argc, argv, "i:o:")) != -1){
+		switch (c) {
+			case 'o':
+				outputFile = optarg;
+				break;
+			case 'i':
+				inputFile  = optarg;
+				break;
+			case '?':
+				if (optopt == 'i'|| optopt == 'o'){
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				}else if (isprint (optopt)){
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				}else{
+					fprintf (stderr,"Unknown option character `\\x%x'.\n",optopt);
+				}
+				return 1;
+			default:
+				abort ();
+		}
+	}
+	if(inputFile == NULL){
+		fprintf(stderr, "-i  must be included\n");
+		return 1;
+	}
+
+	return 0;
+}
