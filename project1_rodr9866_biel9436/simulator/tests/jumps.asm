e15b53211e4577f140597362985647ba1f8c4bc1
rodr9866 <60794335+rodr9866@users.noreply.github.com>
2020-03-13 01:03:59 -0500

    tests for simulator

---
+++ project1_rodr9866_biel9436/simulator/tests/jumps.asm
@@ -0,0 +1,9 @@
+lw 1 0 five
+lw 2 0 two
+jalr 4 1
+add 3 0 1
+beq 0 0 end
+add 3 0 2 # if jalr worked, then register 3 should end as a 2
+end halt # if beq worked then it should halt on 6 instructions
+two	.fill 2
+five	.fill 5
