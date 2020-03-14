6186cca797ffd1276d38f17bb39dacd0de66ab85
rodr9866 <60794335+rodr9866@users.noreply.github.com>
2020-03-13 22:35:19 -0500

    fixed registers

--- project1_rodr9866_biel9436/multiply.asm
+++ project1_rodr9866_biel9436/multiply.asm
@@ -4,17 +4,18 @@
 	lw 6 0 zero	#accumulator must be less than 15
 	lw 7 0 zero #product result
 start	lw 4 0 min
-	nand 5 2 4	 #isolates significant bit
+	nand 5 2 3	 #isolates significant bit
 	nand 5 5 4		#isolates significant bit
-	beq 0 5 if	 #checks if bit is 0, if yes, shifts mcand but does not add to product
+	beq 0 5 true	#checks if bit is 0, if yes, shifts mcand but does not add to product
 	add 7 7 1 	#adds mcand to product
-if	add 1 1 1 	#mcand <<1
+true	add 1 1 1 	#mcand <<1
 	add 3 3 3	#comparator <<1
 	lw 4 0 one	#increments counter
 	add 6 6 4 	#increments counter
 	lw 4 0 fifteen #loads 15 to be checked
 	beq 6 4 end 	#checks if run 15 times
 	beq 0 0 start	#loops back to start
+	end halt
 mcand	 .fill 29562
 mplier	 .fill 11834
 one	 .fill 1
